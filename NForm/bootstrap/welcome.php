ac1f519b3db6ff354f0b1e4abe0d2ff3e6cfe5ed
VikoVoila <32002058+VikoVoila@users.noreply.github.com>
2017-12-20 16:17:44 +0530

    Add files via upload

---
+++ NForm/bootstrap/welcome.php
@@ -0,0 +1,34 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['email']) || empty($_SESSION['email'])){
+  header("location: login.php");
+  exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Hi, <b><?php echo $_SESSION['email']; ?></b>. Welcome to this page.</h1>
+    </div>
+    <p><a href="login.php" class="btn btn-danger">Sign Out</a></p>
+</body>
+</html>
+
+
+
+
+
+<!of Your Account
